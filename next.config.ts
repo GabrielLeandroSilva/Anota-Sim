aeb3234bada61420b38edc0bd51d861ccd903ed9
Gabriel Leandro <gleandro0795@gmail.com>
2026-01-17 23:18:29 -0300

    Add PWA resource

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  //força webpack e desativa turbopack
+  turbopack: {},
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,14 @@ import "./globals.css";
 import { NavigationProvider } from "../providers/NavigationProvider";
 import { ThemeToggleProvider } from "../providers/ThemeToggleProvider";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "AnotaSim",
   description: "Seu app simples para organizar o dia",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/icon-192.png",
+  },
 };
 
 export default function RootLayout({
@@ -14,14 +19,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-<html lang="pt-BR" suppressHydrationWarning>
-  <body className="antialiased bg-gray-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-50">
-    <ThemeToggleProvider>
-      <NavigationProvider>
-        {children}
-      </NavigationProvider>
-    </ThemeToggleProvider>
-  </body>
-</html>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <meta name="theme-color" content="#009767" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </head>
+      <body className="antialiased bg-gray-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-50">
+        <ThemeToggleProvider>
+          <NavigationProvider>
+            {children}
+          </NavigationProvider>
+        </ThemeToggleProvider>
+      </body>
+    </html>
   );
 }
